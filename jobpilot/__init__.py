eb7066b284247c23dae673ffa90311d10453a251
fabifont <me@fabifont.dev>
2023-11-04 17:10:58 +0100

    refactor: add README.md; add simple usage example; better logging

--- jobpilot/__init__.py
+++ jobpilot/__init__.py
@@ -1,3 +1,20 @@
+from __future__ import annotations
+
+import sys
 from importlib.metadata import version
 
+from loguru import logger
+
 __version__ = version("jobpilot")
+
+
+logger.disable("jobpilot")
+
+
+def enable_logging(level: str | int = "INFO") -> None:
+    logger.enable("jobpilot")
+    logger.remove()
+    logger.add(
+        sys.stderr,
+        level=level,
+    )
